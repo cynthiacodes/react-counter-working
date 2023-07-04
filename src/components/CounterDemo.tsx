20755d1857fba9e97a1eb0052c3340b2894a98c3
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-04 14:37:40 +0100

    moved addition & subtraction buttons above the thematic break

--- src/components/CounterDemo.tsx
+++ src/components/CounterDemo.tsx
@@ -18,11 +18,11 @@ function CounterDemo(): JSX.Element {
       <h1>Counter Demo</h1>
       <p>Current value: {counterValueFromCurrentRender}</p>
       <button onClick={handleAddOne}>+1</button>
-      <hr />
-      <button onClick={handleReset}>Reset counter</button>
       <button onClick={() => queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 5)}>-5</button>
       <button onClick={() => queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 1)}>-1</button>
       <button onClick={() => queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 5)}>+5</button>
+      <hr />
+      <button onClick={handleReset}>Reset counter</button>
     </>
   );
 }
