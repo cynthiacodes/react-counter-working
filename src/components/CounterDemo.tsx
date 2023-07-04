6ff5ba6a90151b626438521cb574b24096b77e11
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-04 15:07:56 +0100

    changed re-render value to depend on previous value instead of currentvalue being rendered

--- src/components/CounterDemo.tsx
+++ src/components/CounterDemo.tsx
@@ -5,7 +5,7 @@ function CounterDemo(): JSX.Element {
     useState(0);
 
   const handleAddOne = () => {
-    queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
+    queueRerenderWithNewCounterValue(previousValue => previousValue + 1);
   };
 
   const handleReset = () => {
@@ -18,9 +18,9 @@ function CounterDemo(): JSX.Element {
       <h1>Counter Demo</h1>
       <p>Current value: {counterValueFromCurrentRender}</p>
       <button onClick={handleAddOne}>+1</button>
-      <button onClick={() => queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 5)}>-5</button>
-      <button onClick={() => queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 1)}>-1</button>
-      <button onClick={() => queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 5)}>+5</button>
+      <button onClick={() => queueRerenderWithNewCounterValue(previousValue => previousValue - 5)}>-5</button>
+      <button onClick={() => queueRerenderWithNewCounterValue(previousValue => previousValue - 1)}>-1</button>
+      <button onClick={() => queueRerenderWithNewCounterValue(previousValue => previousValue + 5)}>+5</button>
       <hr />
       <button onClick={handleReset}>Reset counter</button>
     </>
